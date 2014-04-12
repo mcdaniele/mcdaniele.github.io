e34b11bb4266482cb92f7b104c046de904548ca1
Eric McD <mcdaniele@gmail.com>
2014-04-12 14:51:52 -0500

    up45

--- js/menu.js
+++ js/menu.js
@@ -8,17 +8,28 @@ Game.Menu.prototype = {
 		var bgmenu = this.game.add.sprite(42, 0, 'menubg');
 		var playbutton = this.game.add.button(300, 200, 'playb', this.startPlay, this);
 		playbutton.anchor.setTo(0.5, 0);
-		var controllabel = game.add.text(300, 135, 'Touch left half of game screen to move left', { font: '18px Arial', fill: '#000', align: 'left' });
-		controllabel.anchor.setTo(0.5, 0.5);
-		var controllabel2 = game.add.text(300, 160, 'Right half to move right', { font: '18px Arial', fill: '#000', align: 'left' });
-		controllabel2.anchor.setTo(0.5, 0.5);
+		var oldhigh = Number(getCookie("johnnyhigh"));
+		if (highscore>oldhigh) {
+			setCookie("johnnyhigh",highscore,"200");
+		}
+		else {
+			highscore=oldhigh;
+			previoushigh=oldhigh;
+		}
 		var labelscore = game.add.text(80, 50, 'Score: '+score, { font: '18px Arial', fill: '#000', align: 'left' });
 		var labelhighscore = game.add.text(80, 80, 'Previous Highscore: '+previoushigh, { font: '18px Arial', fill: '#000', align: 'left' });
-//		if (game.device.desktop) {
-//			var label = game.add.text(w/2, h-70, '(Or press the up arrow)', { font: '20px Arial', fill: '#000' });
-//			label.anchor.setTo(0.5, 0.5);
-//		}
-//		game.add.text(w-150, 50, 'Music', { font: '18px Arial', fill: '#000', align: 'right' });
+		if (game.device.desktop) {
+			var label = game.add.text(300, 135, 'Arrows to control', { font: '18px Arial', fill: '#000', align: 'center' });
+			label.anchor.setTo(0.5, 0.5);
+			var label22 = game.add.text(300, 160, '(UP arrow to start)', { font: '18px Arial', fill: '#000', align: 'center' });
+			label22.anchor.setTo(0.5, 0.5);
+		}
+		else {
+			var controllabel = game.add.text(300, 135, 'Touch left half of game screen to move left', { font: '18px Arial', fill: '#000', align: 'center' });
+			controllabel.anchor.setTo(0.5, 0.5);
+			var controllabel2 = game.add.text(300, 160, 'Right half to move right', { font: '18px Arial', fill: '#000', align: 'center' });
+			controllabel2.anchor.setTo(0.5, 0.5);
+		}
 		this.music_toggle = this.game.add.button(w-168, 50, 'music', this.toggle_music, this);
 		if (musicbool) {
 			this.music_toggle.frame = 0;
@@ -26,7 +37,6 @@ Game.Menu.prototype = {
 		else {
 			this.music_toggle.frame = 1;
 		}
-//		game.add.text(w-138, 80, 'SFX', { font: '18px Arial', fill: '#000', align: 'right' });
 		this.sfx_toggle = this.game.add.button(w-143, 80, 'sfx', this.toggle_sfx, this);
 		if (sfxbool) {
 			this.sfx_toggle.frame = 0;
