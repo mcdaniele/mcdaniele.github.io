d269c0cab43d5c2b109212a57b528ce2e69f7c80
mcdaniele <mcdaniele@gmail.com>
2015-12-25 19:36:47 -0600

    Update tower.js

--- johnny_tower/js/tower.js
+++ johnny_tower/js/tower.js
@@ -786,7 +786,7 @@ mapz=level.map;
       this.debug         = Dom.get('debug');
       this.score         = Dom.get('score');
       this.vscore        = 0;
-      dom.set(this.score, cc);
+      Dom.set(this.score, cc);
       this.platformWidth = 2 * tower.or * Math.tan((360/tower.cols) * Math.PI / 360);
     },
 
